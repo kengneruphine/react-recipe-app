c4ff88d251715af36c3b1b3fe8f48162654c0def
kengne ruphine <ruphinekengne@gmail.com>
2020-02-21 16:32:47 +0100

    Updates

---
+++ precache-manifest.65cdd91ab341fe28727a702a70a751c9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cf60f1b37369e04bf7439089ffd7653",
+    "url": "/react-recipe-app/index.html"
+  },
+  {
+    "revision": "d4af455b518d2470fdd1",
+    "url": "/react-recipe-app/static/css/main.a09aaeda.chunk.css"
+  },
+  {
+    "revision": "fcc55d8138ed5d792e8d",
+    "url": "/react-recipe-app/static/js/2.d4647771.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/react-recipe-app/static/js/2.d4647771.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4af455b518d2470fdd1",
+    "url": "/react-recipe-app/static/js/main.a92ec147.chunk.js"
+  },
+  {
+    "revision": "8220a2486d1274b3c6c9",
+    "url": "/react-recipe-app/static/js/runtime-main.dbc2b826.js"
+  }
+]);
